c7bde332e62ac2ee5c4f135b487093d2cc81e78a
abantej <abantej@users.noreply.github.com>
2021-10-22 23:36:01 +0800

    Added OpenAPI 3.0 Specification

--- src/main/java/com/abantej/web/controller/ProfileController.java
+++ src/main/java/com/abantej/web/controller/ProfileController.java
@@ -1,4 +1,9 @@
 package com.abantej.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProfileController {
+
 }
